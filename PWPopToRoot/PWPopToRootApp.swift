067d1418befb13d095cd44e4540f6c4ecc66fe4c
Eliezer Sant Ana <er.7santana@gmail.com>
2024-10-02 17:08:03 -0300

    Initial Commit

---
+++ PWPopToRoot/PWPopToRootApp.swift
@@ -0,0 +1,17 @@
+//
+//  PWPopToRootApp.swift
+//  PWPopToRoot
+//
+//  Created by Eliezer Rodrigo on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PWPopToRootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
